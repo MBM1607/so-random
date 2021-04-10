73c9bf689d4571e2112b4659c6bea69e9e33bd78
mbm_1607 <muhammadkhan1607@gmail.com>
2021-04-10 15:02:19 +0500

    Make inputs controlled and add basic generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from 'react';
 import { LoremIpsum } from 'lorem-ipsum';
 
 import './App.css';
 
 const App = () => {
+	const types = ['paragraph', 'sentence', 'word', 'list'];
+	const [type, setType] = useState(0);
+	const [number, setNumber] = useState(4);
+	const [text, setText] = useState('');
 	const lorem = new LoremIpsum({
 		sentencesPerParagraph: {
 			max: 8,
@@ -14,48 +19,54 @@ const App = () => {
 		}
 	});
 
+	const generate = () => {
+		if (type === 0) {
+			setText(lorem.generateParagraphs(number));
+		}
+		else if (type === 1) {
+			setText(lorem.generateSentences(number));
+		}
+		else if (type === 2) {
+			setText(lorem.generateWords(number));
+		}
+		else if (type === 3) {
+			setText(lorem.generateSentences(number));
+		}
+	}
+
+	const changeNumber = (e) => {
+		setNumber(parseInt(e.target.value));
+	};
+
+	const changeType = (e) => {
+		setType(parseInt(e.target.value));
+	};
+
+	useEffect(() => {
+		setText(lorem.generateParagraphs(number));
+	}, []);
+
 	return (
 		<>
 			<header id='top-bar'>
 				<div>
-					<input type='number' name='number' id='number' min='1' />
-					<select name='generate-options' id='generate-options'>
-						<option value='paragraph'>Paragraph</option>
-						<option value='sentences'>Sentences</option>
-						<option value='words'>Words</option>
-						<option value='lists'>Lists</option>
+					<input type='number' name='number' id='number' min='1' value={number} onChange={changeNumber} />
+					<select name='generate-options' id='generate-options' value={type} onChange={changeType} >
+						{
+							types.map((type, index) => {
+								return (
+									<option key={index} value={index}>{type}</option>
+								)
+							})
+						}
 					</select>
-					<button id='generate-btn' className='btn'>Generate!</button>
+					<button id='generate-btn' className='btn' onClick={generate}>Generate!</button>
 				</div>
 				<button id='copy-btn' className='btn'>Copy</button>
 			</header>
 
 			<main id='text-container'>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore magna aliqua. Purus in massa tempor nec feugiat
-					nisl. Tortor dignissim convallis aenean et. Maecenas volutpat blandit aliquam
-					etiam erat. Arcu risus quis varius quam quisque id diam vel. At ultrices mi
-					tempus imperdiet nulla malesuada. Condimentum lacinia quis vel eros donec ac
-					odio tempor. Non nisi est sit amet facilisis magna. Eros donec ac odio tempor.
-					Id eu nisl nunc mi ipsum. Feugiat scelerisque varius morbi enim nunc. Interdum
-					varius sit amet mattis vulputate enim nulla aliquet porttitor. Nullam vehicula
-					ipsum a arcu cursus vitae congue mauris rhoncus. Scelerisque viverra mauris in
-					aliquam sem fringilla ut morbi. Neque viverra justo nec ultrices dui sapien
-					eget. Non sodales neque sodales ut etiam. Turpis nunc eget lorem dolor sed
-					viverra ipsum.
-				</p>
-
-				<ul>
-					<li>Non sodales neque sodales ut etiam. Turpis nunc eget lorem dolor sed
-					viverra ipsum.</li>
-					<li>Non sodales neque sodales ut etiam. Turpis nunc eget lorem dolor sed
-					viverra ipsum.</li>
-					<li>Non sodales neque sodales ut etiam. Turpis nunc eget lorem dolor sed
-					viverra ipsum.</li>
-					<li>Non sodales neque sodales ut etiam. Turpis nunc eget lorem dolor sed
-					viverra ipsum.</li>
-				</ul>
+				{text}
 			</main>
 		</>
 	);
